3c4bfbe1b7eedb72e3373d0042bb452bf4d3453c
Marcelo De Lima <delima_marcelo@outlook.com>
2022-06-06 18:16:21 -0300

    Exercicio 04 Finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../pages';
+import renderWithRouter from './renderWitchRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto', () => {
+    renderWithRouter(<NotFound />);
+    const URL_IMG = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByAltText(/pikachu crying because the page requested was not/i);
+    const notFound = screen.getByText(/page requested not found/i);
+    expect(notFound).toBeInTheDocument();
+    expect(img.src).toBe(URL_IMG);
+  });
+});
