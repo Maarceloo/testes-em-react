57a4a5db4338b34683a13ed900ab9797aa279494
Marcelo De Lima <delima_marcelo@outlook.com>
2022-06-06 18:26:11 -0300

    testando

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import React from 'react';
+// import App from '../App';
+// import renderWithRouter from './renderWitchRouter';
+
+// describe('Teste o componente <Pokedex.js />', () => {
+//   test('Teste se a página contém um heading h2', () => {
+//     renderWithRouter(<App />);
+//     const heading = screen.getByRole('heading', {
+//       name: /encountered pokémons/i,
+//     });
+//     expect(heading).toBeInTheDocument();
+//   });
+//   test('Teste se é exibido o próximo pokémon da lista quando o botão é clicado', () => {
+//       renderWithRouter(<App />);
+
+//   });
+// });
